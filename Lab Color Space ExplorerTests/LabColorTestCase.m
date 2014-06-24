65d13aaad782cc5d09770f4324314576531ff125
jesseilev <jesseilev@gmail.com>
2014-06-24 00:05:10 -0500

    refactor LabColorTestCase setup

--- Lab Color Space ExplorerTests/LabColorTestCase.m
+++ Lab Color Space ExplorerTests/LabColorTestCase.m
@@ -12,9 +12,7 @@
 @interface LabColorTestCase : XCTestCase
 
 @property (nonatomic) LabColor *lColor;
-@property (nonatomic) NSArray *labValues1;
-@property (nonatomic) NSArray *xyzValues1;
-@property (nonatomic) NSArray *rgbValues1;
+@property (nonatomic) NSDictionary *colorDict1;
 
 @end
 
@@ -25,47 +23,50 @@
     // Put setup code here. This method is called before the invocation of each test method in the class.
     NSLog(@"%s", __PRETTY_FUNCTION__);
     self.lColor = [LabColor new];
-    self.labValues1 = @[ @(76.37), @(21.18), @(74.94) ];
-    self.xyzValues1 = @[ @(55.925), @(50.942), @(8.041) ];
-    self.rgbValues1 = @[ @(0.99826), @(0.67160), @(0.11850) ];
+    self.colorDict1 = @{
+                    @"l":@(76.37),  @"a":@(21.18),  @"b":@(74.94),
+                    
+                    @"x":@(55.925), @"y":@(50.942), @"z":@(8.041),
+                    
+                    @"red":@(0.99826), @"green":@(0.67160), @"blue":@(0.11850)
+                        };
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     NSLog(@"%s", __PRETTY_FUNCTION__);
     self.lColor = nil;
-    self.labValues1 = nil;
-    self.rgbValues1 = nil;
+    self.colorDict1 = nil;
     [super tearDown];
+}
+
+- (void)testXYZValues1
+{
+    self.lColor.lComponent = [self.colorDict1[@"l"] floatValue];
+    self.lColor.aComponent = [self.colorDict1[@"a"] floatValue];
+    self.lColor.bComponent = [self.colorDict1[@"b"] floatValue];
+    float accuracy = 3.0; // 3% of range [0.0 thru 100.0]
+    CGFloat xExpected = [self.colorDict1[@"x"] floatValue];
+    XCTAssertEqualWithAccuracy(self.lColor.xComponent, xExpected, accuracy, @"Incorrect X");
+    CGFloat yExpected = [self.colorDict1[@"y"] floatValue];
+    XCTAssertEqualWithAccuracy(self.lColor.yComponent, yExpected, accuracy, @"Incorrect Y");
+    CGFloat zExpected = [self.colorDict1[@"z"] floatValue];
+    XCTAssertEqualWithAccuracy(self.lColor.zComponent, zExpected, accuracy, @"Incorrect Z");
 }
 
 - (void)testRGBValues1
 {
     NSLog(@"%s doing work...", __PRETTY_FUNCTION__);
-    self.lColor.lComponent = [self.labValues1[0] floatValue];
-    self.lColor.aComponent = [self.labValues1[1] floatValue];
-    self.lColor.bComponent = [self.labValues1[2] floatValue];
-    float accuracy = 0.06;
-    CGFloat rExpected = [self.rgbValues1[0] floatValue];
+    self.lColor.lComponent = [self.colorDict1[@"l"] floatValue];
+    self.lColor.aComponent = [self.colorDict1[@"a"] floatValue];
+    self.lColor.bComponent = [self.colorDict1[@"b"] floatValue];
+    float accuracy = 0.06; //6% of range [0.0 thru 1.0]
+    CGFloat rExpected = [self.colorDict1[@"red"] floatValue];
     XCTAssertEqualWithAccuracy(self.lColor.redComponent, rExpected, accuracy, @"Incorrect Red");
-    CGFloat gExpected = [self.rgbValues1[1] floatValue];
+    CGFloat gExpected = [self.colorDict1[@"green"] floatValue];
     XCTAssertEqualWithAccuracy(self.lColor.greenComponent, gExpected, accuracy, @"Incorrect Green");
-    CGFloat bExpected = [self.rgbValues1[2] floatValue];
+    CGFloat bExpected = [self.colorDict1[@"blue"] floatValue];
     XCTAssertEqualWithAccuracy(self.lColor.blueComponent, bExpected, accuracy, @"Incorrect Blue");
-}
-
-- (void)testXYZValues1
-{
-    self.lColor.lComponent = [self.labValues1[0] floatValue];
-    self.lColor.aComponent = [self.labValues1[1] floatValue];
-    self.lColor.bComponent = [self.labValues1[2] floatValue];
-    float accuracy = 3.0;
-    CGFloat xExpected = [self.xyzValues1[0] floatValue];
-    XCTAssertEqualWithAccuracy(self.lColor.xComponent, xExpected, accuracy, @"Incorrect X");
-    CGFloat yExpected = [self.xyzValues1[1] floatValue];
-    XCTAssertEqualWithAccuracy(self.lColor.yComponent, yExpected, accuracy, @"Incorrect Y");
-    CGFloat zExpected = [self.xyzValues1[2] floatValue];
-    XCTAssertEqualWithAccuracy(self.lColor.zComponent, zExpected, accuracy, @"Incorrect Z");
 }
 
 - (void)testPerformanceExample {
